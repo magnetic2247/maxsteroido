1e34e899967ace30414d078c12c09def0dbee4f7
Polis Minus <polisminus2247@tuta.io>
2023-12-04 21:16:31 +0100

    Better build script

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 mkdir -p bin
-cd src/
-g++ -lm -lsfml-graphics -lsfml-window -lsfml-system main.cpp entities.cpp -o ../bin/maxsteroid
+g++ -lm -lsfml-graphics -lsfml-window -lsfml-system src/main.cpp src/entities.cpp -o bin/maxsteroid
