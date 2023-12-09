04943f6866ab0f695623634f2dd38945bc1f5bff
Polis Minus <polisminus2247@tuta.io>
2023-12-09 15:40:40 +0100

    optimize binaries

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p release/linux_arm64
-g++ src/main.cpp src/game.cpp src/entities.cpp -lm -lsfml-graphics -lsfml-window -lsfml-system -o release/linux_arm64/maxsteroid
+mkdir -p release/linux_amd64
+g++ src/main.cpp src/game.cpp src/entities.cpp -lm -lsfml-graphics -lsfml-window -lsfml-system -Ofast -o release/linux_amd64/maxsteroid
--- build_win.sh
+++ build_win.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 mkdir -p release/windows/
-x86_64-w64-mingw32-g++ src/main.cpp src/game.cpp src/entities.cpp -D SFML_STATIC -D M_PIl=M_PI -Ilibraries/include/ -Ilibraries/SFML/include/ -Llibraries/SFML/lib/ -static -lm -lsfml-graphics-s -lfreetype -lsfml-window-s -lopengl32 -lgdi32 -lsfml-system-s -lwinmm -o release/windows/maxsteroid.exe
+x86_64-w64-mingw32-g++ \
+	src/main.cpp src/game.cpp src/entities.cpp \
+	-D SFML_STATIC -D M_PIl=M_PI \
+	-Ilibraries/include/ -Ilibraries/SFML/include/ -Llibraries/SFML/lib/ \
+	-static -lm -lsfml-graphics-s -lfreetype -lsfml-window-s -lopengl32 -lgdi32 -lsfml-system-s -lwinmm \
+	-Ofast -o release/windows/maxsteroid.exe
